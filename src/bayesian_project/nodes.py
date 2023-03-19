167c5bcc52b8d9bb9daf52492268186bf934c07e
samuel <samueloliveira32df@gmail.com>
2023-03-19 18:20:36 -0300

    bayesian_test (feat): create node to vary between test sites

--- src/bayesian_project/nodes.py
+++ src/bayesian_project/nodes.py
@@ -7,33 +7,40 @@ import logging
 from typing import Any, Dict, Tuple
 
 import numpy as np
-import pandas as pd
+from flask import Flask, redirect, render_template, url_for
 
+app = Flask(__name__, template_folder="../../data/01_raw")
 
 def split_data(
     data: pd.DataFrame, parameters: Dict[str, Any]
 ) -> Tuple[pd.DataFrame, pd.DataFrame, pd.Series, pd.Series]:
     """Splits data into features and target training and test sets.
 
-    Args:
-        data: Data containing features and target.
-        parameters: Parameters defined in parameters.yml.
-    Returns:
-        Split data.
-    """
+@app.route("/home")
+def index():
+    if np.random.random() < 0.5:
+        selected_template = render_template("pg_layout_blue.html")
+    else:
+        selected_template = render_template("pg_layout_red.html")
+    return selected_template
 
     data_train = data.sample(
         frac=parameters["train_fraction"], random_state=parameters["random_state"]
     )
     data_test = data.drop(data_train.index)
 
-    X_train = data_train.drop(columns=parameters["target_column"])
-    X_test = data_test.drop(columns=parameters["target_column"])
-    y_train = data_train[parameters["target_column"]]
-    y_test = data_test[parameters["target_column"]]
+@app.route("/yes", methods=["POST"])
+def yes_event():
+    return redirect(url_for("index"))
 
-    return X_train, X_test, y_train, y_test
 
+@app.route("/no", methods=["POST"])
+def no_event():
+    return redirect(url_for("index"))
+
+
+def run_flask_app():
+    app.run()
 
 def make_predictions(
     X_train: pd.DataFrame, X_test: pd.DataFrame, y_train: pd.Series
